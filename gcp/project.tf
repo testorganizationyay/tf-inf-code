7bb8beb68b4491b4fd8553550d248ead6a4cd7a4
noname <grrrr@protonmail.com>
2019-07-10 17:22:11 +0300

    Working tf file

--- gcp/project.tf
+++ gcp/project.tf
@@ -1,13 +1,31 @@
-variable "project" {
-  type = string
+variable "project_name" {}
+variable "billing_account" {}
+variable "org_id" {}
+variable "region" {}
+
+provider "google" {
+ region = "${var.region}"
 }
 
-variable "google_project" {
-  type = string
+resource "random_id" "id" {
+ byte_length = 4
+ prefix      = "${var.project_name}-"
 }
 
-data "google_project" "project" {}
+resource "google_project" "project" {
+ name            = "${var.project_name}"
+ project_id      = "${random_id.id.hex}"
+ billing_account = "${var.billing_account}"
+ org_id          = "${var.org_id}"
+}
 
-output "project_number" {
-  value = "${data.google_project.project.number}"
+resource "google_project_services" "project" {
+ project = "${google_project.project.project_id}"
+ services = [
+   "compute.googleapis.com"
+ ]
+}
+
+output "project_id" {
+ value = "${google_project.project.project_id}"
 }
