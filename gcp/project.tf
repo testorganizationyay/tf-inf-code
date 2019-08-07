6bda194e94ca895310a026d0c95b08af290798f1
noname <grrrr@protonmail.com>
2019-08-07 10:44:00 +0300

    Add folder_id

--- gcp/project.tf
+++ gcp/project.tf
@@ -1,6 +1,7 @@
 variable "project_name" {}
 variable "billing_account" {}
 variable "org_id" {}
+variable "folder_id" {}
 variable "region" {}
 
 provider "google" {
@@ -16,7 +17,6 @@ resource "google_project" "project" {
  name            = "${var.project_name}"
  project_id      = "${random_id.id.hex}"
  billing_account = "${var.billing_account}"
- org_id          = "${var.org_id}"
 }
 
 resource "google_project_services" "project" {
