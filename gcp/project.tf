de508ee9b5fa8bb4a5392ecbae1456534db8e9f2
noname <grrrr@protonmail.com>
2019-08-07 11:22:41 +0300

    Remove random project_id

--- gcp/project.tf
+++ gcp/project.tf
@@ -10,14 +10,9 @@ provider "google" {
  region = "${var.region}"
 }
 
-resource "random_id" "id" {
- byte_length = 4
- prefix      = "${var.project_name}-"
-}
-
 resource "google_project" "project" {
  name            = "${var.project_name}"
- project_id      = "${random_id.id.hex}"
+ project_id      = "${var.project_id}"
  billing_account = "${var.billing_account}"
 }
 
