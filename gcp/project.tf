d0d4caeaa0325a8090a11ced47d67425cd31a06c
noname <eliredman@bigmir.net>
2019-12-19 13:12:17 +0200

    Another update

--- gcp/project.tf
+++ gcp/project.tf
@@ -17,6 +17,7 @@ resource "google_project" "project" {
  name            = "${var.project_name}"
  project_id      = "${var.project_id}"
  billing_account = "${var.billing_id}"
+ folder_id       = "${var.folder_id}"
 }
 
 resource "google_project_service" "project" {
