8c772ae108314f0abd7ec6ce17e46181ed7b0942
noname <eliredman@bigmir.net>
2019-12-26 15:10:13 +0200

    Upd

--- gcp/project.tf
+++ gcp/project.tf
@@ -1,5 +1,4 @@
 variable "project_id" {}
-variable "role_bindings" {}
 variable "project_name" {}
 variable "org_id" {}
 variable "folder_id" {}
@@ -7,18 +6,19 @@ variable "skip_delete" {}
 variable "billing_id" {}
 
 resource "google_project" "project" {
- name            = "${var.project_name}"
- project_id      = "${var.project_id}"
- billing_account = "${var.billing_id}"
- folder_id       = "${var.folder_id}"
+ name            = var.project_name
+ project_id      = var.project_id
+ billing_account = var.billing_id
+ folder_id       = var.folder_id
+ skip_delete     = var.skip_delete
 }
 
 resource "google_project_service" "project" {
-  project = "${google_project.project.project_id}"
+  project = google_project.project.project_id
   service = "compute.googleapis.com"
   disable_on_destroy = false
 }
 
 output "project_id" {
- value = "${google_project.project.project_id}"
+ value = google_project.project.project_id
 }
